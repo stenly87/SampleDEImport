fb103dd2f1aed3aef31d6cbf862f21a64b782e8c
Alex Pushkin <ifc@suz-ppk.ru>
2022-11-25 09:55:49 +1000

    update

--- Стройматериалы/ViewModel/ProductListViewModel.cs
+++ Стройматериалы/ViewModel/ProductListViewModel.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media.Imaging;
 using Стройматериалы.DB;
 using Стройматериалы.Models;
@@ -17,8 +18,42 @@ namespace Стройматериалы.ViewModel
         private User user;
         private MainViewModel mainViewModel;
         private List<Product> products;
+        private string search = "";
+        private ProductManufacturer selectedManufacturer;
+        private string sortPrice;
 
         public List<ProductManufacturer> Manufacturers { get; set; }
+        public ProductManufacturer SelectedManufacturer
+        {
+            get => selectedManufacturer;
+            set
+            {
+                selectedManufacturer = value;
+                DoSearch();
+            }
+        }
+        public string Search
+        {
+            get => search;
+            set
+            {
+                search = value;
+                DoSearch();
+            }
+        }
+        bool sortPriceAsc = false;
+        private string rowsCount;
+
+        public string SortPrice
+        {
+            get => sortPrice;
+            set
+            {
+                sortPrice = value;
+                sortPriceAsc = sortPrice.Contains("По возрастанию");
+                DoSearch();
+            }
+        }
 
         public List<Product> Products
         {
@@ -29,6 +64,20 @@ namespace Стройматериалы.ViewModel
                 SignalChanged();
             }
         }
+        public Product SelectedProduct { get; set; }
+        public string RowsCount
+        {
+            get => rowsCount;
+            set
+            {
+                rowsCount = value;
+                SignalChanged();
+            }
+        }
+
+        public Visibility IsAdminVisibility { get => user.UserRole == 1 ? Visibility.Visible : Visibility.Collapsed; }
+
+        public ViewCommand RemoveProduct { get; set; }
 
         public ProductListViewModel(User user, MainViewModel mainViewModel)
         {
@@ -37,26 +86,86 @@ namespace Стройматериалы.ViewModel
 
             Manufacturers = DEContext.GetInstance().ProductManufacturers.ToList();
             Manufacturers.Insert(0, new ProductManufacturer { Title = "Все производители" });
-            Search();
+            SelectedManufacturer = Manufacturers[0];
+
+            RemoveProduct = new ViewCommand(() => {
+                if (SelectedProduct == null)
+                {
+                    MessageBox.Show("Необходимо выбрать товар из списка для его удаления");
+                    return;
+                }
+                if (SelectedProduct.OrderProducts.Count > 0)
+                {
+                    MessageBox.Show("Выбранный товар нельзя удалить, поскольку он участвует в заказах");
+                    return;
+                }
+                try
+                {
+                    DEContext.GetInstance().Products.Remove(SelectedProduct);
+                    DEContext.GetInstance().SaveChanges();
+                    DoSearch(); // либо удалять из Products в вьюмодели и использовать ObservableCollection
+                    MessageBox.Show("Выбранный товар удален");
+                }
+                catch
+                {
+                    MessageBox.Show("Произошла ошибка при удалении товара");
+                }
+            });
         }
 
-        private void Search()
+        private void DoSearch()
         {
-            Products = DEContext.GetInstance().Products
-                            .Include("ProductManufacturer")
-                            .Include("ProductUnit")
-                            .Include("Supplier")
-                            .Include("ProductCategory").ToList();
-            
-            Products.ForEach(s =>
+            try
             {
-                s.Image = new BitmapImage();
-                s.Image.BeginInit();
-                s.Image.CacheOption = BitmapCacheOption.OnLoad;
-                s.Image.StreamSource = File.OpenRead("picture.png");
-                s.Image.EndInit();
-                s.Image.StreamSource.Close(); // обязательно
-            });
+                var countAll = DEContext.GetInstance().Products.Count();
+
+                var searchProducts = DEContext.GetInstance().Products
+                                .Include("ProductManufacturer")
+                                .Include("ProductUnit")
+                                .Include("Supplier")
+                                .Include("OrderProducts")
+                                .Include("ProductCategory").Where(
+                    s => s.ProductArticleNumber.Contains(Search) ||
+                        s.ProductCategory.Title.Contains(Search) ||
+                        s.ProductDescription.Contains(Search) ||
+                        s.ProductManufacturer.Title.Contains(Search) ||
+                        s.ProductName.Contains(Search) ||
+                        s.Supplier.Title.Contains(Search)
+                        );
+
+                if (SelectedManufacturer.ManufacturerId != 0)
+                    searchProducts = searchProducts.Where(s => s.ProductManufacturerId == SelectedManufacturer.ManufacturerId);
+                if (sortPriceAsc)
+                    searchProducts = searchProducts.OrderBy(s => s.ProductCost);
+                else
+                    searchProducts = searchProducts.OrderByDescending(s => s.ProductCost);
+
+                var prod = searchProducts.ToList();
+
+                prod.ForEach(s =>
+                {
+                    s.Image = new BitmapImage();
+                    s.Image.BeginInit();
+                    s.Image.CacheOption = BitmapCacheOption.OnLoad;
+                    if (s.ProductPhoto != null)
+                    {
+                        var ms = new MemoryStream(s.ProductPhoto);
+                        s.Image.StreamSource = ms;
+                    }
+                    else
+                        s.Image.StreamSource = File.OpenRead("picture.png");
+                    s.Image.EndInit();
+                    s.Image.StreamSource.Close(); // обязательно
+                });
+
+                if (prod.Count == 0)
+                    MessageBox.Show("По данному запросу ничего не найдено");
+
+                RowsCount = $"Получено {prod.Count} из {countAll} записей";
+
+                Products = prod;
+            }
+            catch { }
         }
     }
 }
